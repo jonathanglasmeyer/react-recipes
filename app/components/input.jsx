7f560038994d6a76373fb3b40353d43b1dcdbad3
Jonathan Werner <jwerner@posteo.de>
2014-12-10 12:26:51 +0100

    backup typeahead stuff

--- app/components/input.jsx
+++ app/components/input.jsx
@@ -6,50 +6,7 @@ var Svg = require('components/svg');
 
 var Actions = require('actions');
 
-// var lebensmittel = new Bloodhound({
-//   datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
-//   queryTokenizer: Bloodhound.tokenizers.whitespace,
-//   limit: 5,
-//   prefetch: {
-//     url: './data/lebensmittel.json',
-//     filter: function(list) {
-//       return $.map(list, function(country) { return { name: country }; });
-//     }
-//   }
-// });
-// lebensmittel.initialize();
-
-
-
-    //     $(element).typeahead({
-    //         hint: true,
-    //         highlight: true,
-    //         minLength: 1
-    //     },
-    //     {
-    //       name: 'lebensmittel',
-    //       displayKey: 'name',
-    //       source: lebensmittel.ttAdapter()
-    //     }).focus();
-
-    //     $(element).on('typeahead:selected', (jquery, datum) => {
-    //         this._clearAndSend();
-		// });
-    //     $(element).on('typeahead:autocompleted', (jquery, datum) => {
-    //         this._clearAndSend();
-		// });
-	// },
-
 module.exports = React.createClass({
-    // componentWillUnmount() {
-    //     var element = this.refs.input.getDOMNode();
-		// $(element).typeahead('destroy');
-	// },
-
-    // componentDidMount() {
-    //     element = this.refs.input.getDOMNode();
-    //     $(element).focus();
-    // },
 
     handleSubmit(e) {
         e.preventDefault();
---
+++ codestash/typeahead.es6
@@ -0,0 +1,34 @@
+
+// var lebensmittel = new Bloodhound({
+//   datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+//   queryTokenizer: Bloodhound.tokenizers.whitespace,
+//   limit: 5,
+//   prefetch: {
+//     url: './data/lebensmittel.json',
+//     filter: function(list) {
+//       return $.map(list, function(country) { return { name: country }; });
+//     }
+//   }
+// });
+// lebensmittel.initialize();
+
+
+
+    //     $(element).typeahead({
+    //         hint: true,
+    //         highlight: true,
+    //         minLength: 1
+    //     },
+    //     {
+    //       name: 'lebensmittel',
+    //       displayKey: 'name',
+    //       source: lebensmittel.ttAdapter()
+    //     }).focus();
+
+    //     $(element).on('typeahead:selected', (jquery, datum) => {
+    //         this._clearAndSend();
+		// });
+    //     $(element).on('typeahead:autocompleted', (jquery, datum) => {
+    //         this._clearAndSend();
+		// });
+	// },
