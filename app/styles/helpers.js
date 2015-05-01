f3e41cb19f49a3e3b6771ff8fd44103726886a21
Jonathan Werner <jwerner@posteo.de>
2015-05-01 15:41:49 +0200

    misc styles

--- app/styles/helpers.js
+++ app/styles/helpers.js
@@ -2,8 +2,7 @@ var C = require('color');
 
 export function makeStates(color, alphaHover, alphaFocusActive) {
   return [
-    {hover: {background: C(color).alpha(alphaHover).rgbString()}},
-    {focus: {background: C(color).alpha(alphaFocusActive).rgbString()}},
-    {active: {background: C(color).alpha(alphaFocusActive).rgbString()}}
+    {hover: {background: C(color).alpha(alphaHover).rgbString()}}
   ];
+    // {active: {background: C(color).alpha(alphaFocusActive).rgbString()}}
 }
